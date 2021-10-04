fde460c40dba173c0048f7cc30843440e693caaf
TheRealLN <38964101+TheRealLN@users.noreply.github.com>
2021-10-04 00:06:09 -0400

    Fixed shaping issues with multiple init methods.

--- src/ml_functions/init_methods.py
+++ src/ml_functions/init_methods.py
@@ -17,18 +17,18 @@ def ones(shape: tuple) -> np.ndarray:
 
 def randu(shape: tuple) -> np.ndarray:
     rng = np.random.default_rng()
-    return rng.uniform(shape)
+    return rng.uniform(size=shape)
 
 def randn(shape: tuple) -> np.ndarray:
     rng = np.random.default_rng()
-    return rng.standard_normal(shape)
+    return rng.standard_normal(size=shape)
 
 def he(shape: tuple) -> np.ndarray:
     """Designed for ReLU() activaiton functions"""
     rng = np.random.default_rng()
-    return rng.standard_normal(shape) * np.sqrt(2. / shape[0])
+    return rng.standard_normal(size=shape) * np.sqrt(2. / shape[0])
 
 def xavier(shape: tuple) -> np.ndarray:
     """Designed for tanh() & sigmoid() activation functions (as these are approx linear around 0)"""
     rng = np.random.default_rng()
-    return rng.standard_normal(shape) * np.sqrt(1. / shape[0])
+    return rng.standard_normal(size=shape) * np.sqrt(1. / shape[0])
